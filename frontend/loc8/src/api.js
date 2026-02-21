f605f27ee0fe97dde282bbc9b2bbdf0ab39fc31b
Swarali Pednekar <swaralip1808@gmail.com>
2026-02-22 00:08:18 +0530

    For kriish's shield

--- frontend/loc8/src/api.js
+++ frontend/loc8/src/api.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:3000";
-export const PYTHON_API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "jskloc8w2-production.up.railway.app";
+export const PYTHON_API_BASE_URL = "jskloc8w2-production.up.railway.app";
